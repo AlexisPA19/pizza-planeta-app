8f020a8c5c0b6b0434e90f364a50b704622b1a4e
Alexis <huki_19@hotmail.com>
2019-03-26 11:02:18 -0600

    Alexis
    Carrito de compra

--- src/carrito.js
+++ src/carrito.js
@@ -4,10 +4,40 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class Carrito extends Component {
+  constructor () {
+    super();
+    this.state = {
+        pizzas: [
+            {pizzaId:1, nomPizza:'Hawaiana', descripPizza:'Piña y jamón', tamPiza:'Grande', tipoMasa:'Queso', Precio:200},
+            {pizzaId:2, nomPizza:'Pepperoni', descripPizza:'Queso...', tamPiza:'Chica', tipoMasa:'Queso', Precio:200}
+        ]
+    }
+}
   render() {
     return (
         <div className="Carrito">
-            <h1>El carrito está vacio</h1>
+            {
+                    this.state.pizzas.map(pizza => {
+                        return(
+                            <div className="row">
+                              <div className="col">{pizza.nomPizza}</div>
+                              <div className="col">{pizza.descripPizza}</div>
+                              <div className="col">{pizza.tamPiza}</div>
+                              <div className="col">{pizza.tipoMasa}</div>
+                              <div className="col">{pizza.Precio}</div>
+                              <div className="col"><button className="btn">Eliminar</button></div>
+                            </div>
+                        )
+                    })
+                }
+                <div className="row">
+                  <div className="col">
+                    <Link className="btn" to={'/'}>Seguir comprando</Link>
+                  </div>
+                  <div className="col">
+                    <Link className="btn" to={'/datosCompra'}>Proceder con la compra</Link>
+                  </div>
+                </div>
         </div>
     );
   }
