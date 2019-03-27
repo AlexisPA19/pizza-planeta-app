0096a8a3d089ff39ad7c20dfa88dd4fb09fa8dfb
Alexis <huki_19@hotmail.com>
2019-03-27 13:26:11 -0600

    Formulario domicilio

--- src/datosCompra.js
+++ src/datosCompra.js
@@ -2,21 +2,101 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './datosCompra.css';
 
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="form-group row">
+        <label for="inputEmail4" className="col-sm-2 col-form-label">* Numero</label>
+        <div class="form-group col-md-2">
+            <input type="tel" class="form-control" id="inputEmail4" placeholder=""/>
+        </div>
+        <label for="inputPassword4">* CP</label>
+        <div class="form-group col-md-2">
+            <input type="email" class="form-control" id="inputPassword4" placeholder=""/>
+        </div>
+      </div>
+      <div className="form-group row">
+        <label for="staticEmail" className="col-sm-2 col-form-label">* Colonia</label>
+        <div className="col-sm-10">
+            <input type="text" class="form-control" id="inputEmail4" placeholder=""/>
+        </div>
+      </div>
+      <div className="form-group row">
+        <label for="staticEmail" className="col-sm-2 col-form-label">* Delegación o municipio</label>
+        <div className="col-sm-10">
+            <input type="text" class="form-control" id="inputEmail4" placeholder=""/>
+        </div>
+      </div>
+      <div className="form-group row">
+        <label for="staticEmail" className="col-sm-2 col-form-label">* Estado</label>
+        <div className="col-sm-2">
+          <select className="form-control" >
+            <option value="Aguascalientes">Aguascalientes</option>
+            <option value="Baja California" >Baja California</option>
+            <option value="Baja California Sur" >Baja California Sur</option>
+            <option value="Campeche" >Campeche</option>
+            <option value="Coahuila" >Baja California</option>
+            <option value="Distrito Federal" >Ciudad de México</option>
+            <option value="Coahuila" >Coahuila</option>
+            <option value="Colima" >Colima</option>
+            <option value="Chiapas" >Chiapas</option>
+            <option value="Chihuahua" >Chihuahua</option>
+            <option value="Durango" >Durango</option>
+            <option value="Guanajuato" >Guanajuato</option>
+            <option value="Guerrero" >Guerrero</option>
+            <option value="Hidalgo" >Hidalgo</option>
+            <option value="Jalisco" >Jalisco</option>
+            <option value="Estado de México" >Estado de México</option>
+            <option value="Michoacán" >Michoacán</option>
+            <option value="Morelos" >Morelos</option>
+            <option value="Nayarit" >Nayarit</option>
+            <option value="Nuevo León" >Nuevo León</option>
+            <option value="Oaxaca" >Oaxaca</option>
+            <option value="Puebla" >Puebla</option>
+            <option value="Querétaro" >Querétaro</option>
+            <option value="Quintana Roo" >Quintana Roo</option>
+            <option value="San Luis Potosí" >San Luis Potosí</option>
+            <option value="Sinaloa" >Baja California</option>
+            <option value="Baja California" >Sinaloa</option>
+            <option value="Sonora" >Sonora</option>
+            <option value="Tabasco" >Tabasco</option>
+            <option value="Tamaulipas" >Tamaulipas</option>
+            <option value="Tlaxcala" >Tlaxcala</option>
+            <option value="Veracruz" >Veracruz</option>
+            <option value="Yucatán" >Yucatán</option>
+            <option value="Zacatecas" >Zacatecas</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    
+  );
+}
 class DatosCompra extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'Grande'};
-    
+        this.state = {value: ''};
+
+        this.state = {showWarning: false}
+        this.handleToggleClick = this.handleToggleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
       }
-    
+      handleToggleClick() {
+        this.setState(prevState => ({
+          showWarning: !prevState.showWarning
+        }));
+      }
       handleChange(event) {
-        this.setState({value: event.target.value});
+
       }
     
       handleSubmit(event) {
-        alert('Your favorite flavor is: ' + this.state.value);
         event.preventDefault();
       }
     
@@ -28,10 +108,10 @@ class DatosCompra extends Component {
             <form onSubmit={this.handleSubmit}>
               <div className="form-group row">
                 <label for="staticEmail" className="col-sm-2 col-form-label">* Tipo de entrega</label>
-                <div className="col-sm-10">
-                  <select className="form-control" value={this.state.value} onChange={this.handleChange}>
+                <div className="col-sm-3">
+                  <select className="form-control"  onClick={this.handleToggleClick}>
                     <option value="sucursal">En sucursal</option>
-                    <option value="domicilio" >A domicilio</option>
+                    <option value="domicilio" onClick={this.handleToggleClick}>A domicilio</option>
                   </select>
                 </div>
               </div>
@@ -51,6 +131,7 @@ class DatosCompra extends Component {
                     <input type="email" class="form-control" id="inputPassword4" placeholder="email@dom.dom"/>
                 </div>
               </div>
+                <WarningBanner warn={this.state.showWarning} />
               <div className="form-group row">
                 <div className="col-sm-6">
                   <Link id="boton" className="btn btn-default" to={'/carritoCompra'}>Cancelar</Link>
