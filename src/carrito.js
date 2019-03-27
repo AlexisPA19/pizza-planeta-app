aba7a5f2171db12e17e2bba900af3a5bbf1a7a3f
Alexis <huki_19@hotmail.com>
2019-03-27 16:30:56 -0600

    suma carrito

--- src/carrito.js
+++ src/carrito.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './carrito.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PizzaCarritoComponent from './pizzaCarritoComponent';
+
+var suma =0;
 class Carrito extends Component {
   constructor () {
     super();
     this.state = {
         pizzas: [
-            {pizzaId:1, nomPizza:'Hawaiana', descripPizza:'Piña y jamón', tamPiza:'Grande', tipoMasa:'Queso', Precio:200},
-            {pizzaId:2, nomPizza:'Pepperoni', descripPizza:'Queso...', tamPiza:'Chica', tipoMasa:'Queso', Precio:200},
-            {pizzaId:3, nomPizza:'Pepperoni', descripPizza:'Queso...', tamPiza:'Chica', tipoMasa:'Queso', Precio:200}
+            {pizzaId:1, nomPizza:'Hawaiana', descripPizza:'Piña y jamón', tamPiza:'Grande', tipoMasa:'Queso', cant:1, Precio:200},
+            {pizzaId:2, nomPizza:'Pepperoni', descripPizza:'Queso...', tamPiza:'Chica', tipoMasa:'Queso',cant:2, Precio:200},
+            {pizzaId:3, nomPizza:'Pepperoni', descripPizza:'Queso...', tamPiza:'Chica', tipoMasa:'Queso', cant:1,Precio:200}
         ]
     }
 }
@@ -24,6 +26,7 @@ class Carrito extends Component {
                 <th>Ingredientes</th>
                 <th>Tamaño</th>
                 <th>Masa</th>
+                <th>Cantidad</th>
                 <th>Precio</th>
                 <th></th>
               </tr>
@@ -32,22 +35,26 @@ class Carrito extends Component {
             {
                     this.state.pizzas.map(pizza => {
                         return(
-                                <PizzaCarritoComponent key={pizza.pizzaId} nomPizza={pizza.nomPizza} descripPizza={pizza.descripPizza} tamPizza={pizza.tamPiza} tipoMasa={pizza.tipoMasa} Precio={pizza.Precio}/>
+                          <PizzaCarritoComponent key={pizza.pizzaId} nomPizza={pizza.nomPizza} descripPizza={pizza.descripPizza} tamPizza={pizza.tamPiza} tipoMasa={pizza.tipoMasa} cant={pizza.cant} Precio={pizza.Precio}/>
                         )
+                        suma = suma + pizza.Precio
                     })
-                }
-              </tbody>
-            </table>
-                <div className="row">
-                  <div className="col">
-                    Total
-                  </div>
-                  <div className="col"></div>
-                  <div className="col"></div>
-                  <div className="col"></div>
-                  <div className="col"></div>
-                  <div className="col"></div>
-                </div>
+            }
+            {
+                      
+              this.state.pizzas.map(pizza=>{
+                  suma = suma + pizza.Precio*pizza.cant
+                
+              })
+            }
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Total:</td>
+            <td>{suma}</td>
+            </tbody>
+          </table>
                 <div className="row">
                   <div className="col">
                     <Link id="boton" className="btn btn-default" to={'/'}>Seguir comprando</Link>
--- src/pizzaCarritoComponent.js
+++ src/pizzaCarritoComponent.js
@@ -11,6 +11,7 @@ constructor(props){
     this.descripPizza = props.descripPizza;
     this.tamPizza = props.tamPizza;
     this.tipoMasa = props.tipoMasa;
+    this.cant = props.cant;
     this.precio = props.Precio;
 }
   render() {
@@ -20,6 +21,7 @@ constructor(props){
             <td>{this.descripPizza}</td>
             <td>{this.tamPizza}</td>
             <td>{this.tipoMasa}</td>
+            <td>{this.cant}</td>
             <td>{this.precio}</td>
             <td><button className="btn btn-danger" data-toggle="modal" data-target="#exampleModal" data-backdrop="static">Eliminar</button></td>
             <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
