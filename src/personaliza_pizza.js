d78085466b2f6b46f0a1b85d92a16c5b28edf546
Alexis <huki_19@hotmail.com>
2019-03-27 12:17:04 -0600

    Update personaliza_pizza.js

--- src/personaliza_pizza.js
+++ src/personaliza_pizza.js
@@ -8,19 +8,8 @@ import './personaliza_pizza.css';
 class PersonalizaPizza extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'Grande'};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('Your favorite flavor is: ' + this.state.value);
-        event.preventDefault();
+      
+  
       }
       render() {
         return (
