acb9c0e5d58f5e201fef2c72fbc7658daacb02c3
Alexis <huki_19@hotmail.com>
2019-03-27 12:18:19 -0600

    Update personaliza_pizza.js

--- src/personaliza_pizza.js
+++ src/personaliza_pizza.js
@@ -8,8 +8,19 @@ import './personaliza_pizza.css';
 class PersonalizaPizza extends Component {
     constructor(props) {
         super(props);
-      
-  
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+
+      }
+    
+      handleSubmit(event) {
+
+        event.preventDefault();
       }
       render() {
         return (
